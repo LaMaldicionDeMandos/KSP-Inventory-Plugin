4d66236651096ace701de7b3ae8a74c8dde4c9c9
marcelo <pasutmarcelo@gmail.com>
2020-06-19 02:06:28 -0300

    Primeras cosas, capturo eventos de partes

---
+++ KSP_Inventory/InventoryFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace inventory
+{
+    [KSPAddon(KSPAddon.Startup.EditorVAB, false)]
+    public class InventoryFactory : MonoBehaviour
+    {
+
+        public void Start()
+        {
+            Debug.Log("[INVENTORY]Factory loaded");
+            print("Factory Loaded");
+            GameEvents.onEditorPartEvent.Add(PartEvent);
+        }
+
+        private void PartEvent(ConstructionEventType type, Part part)
+        {
+            Debug.Log("[INVENTORY] Part Event: " + type + ", part: " + part.name + ", model name: " + part.PartValues);
+
+        }
+    }
+}
---
+++ KSP_Inventory/InventoryScenario.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace inventory
+{
+    [KSPScenario(ScenarioCreationOptions.AddToNewCareerGames | ScenarioCreationOptions.AddToExistingCareerGames, GameScenes.EDITOR, GameScenes.PSYSTEM, GameScenes.SPACECENTER, GameScenes.FLIGHT)]
+    public class InventoryScenario : ScenarioModule
+    {
+        public void Update()
+        {
+            ScreenMessages.PostScreenMessage("                El escenario está activo!!", 1);
+        }
+
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            ScreenMessages.PostScreenMessage("                El escenario Vive!!", 3);
+            
+        }
+
+
+
+        public override void OnSave(ConfigNode node)
+        {
+
+            base.OnSave(node);
+            ScreenMessages.PostScreenMessage("        OnSave", 3);
+            print("Saved o sea anda");
+          
+            List<AvailablePart> parts = new List<AvailablePart>();
+            parts.ForEach(delegate (AvailablePart part)
+            {
+                print(part.name);
+            });
+            print("imprimio algo?");
+            
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            ScreenMessages.PostScreenMessage("        OnLoad", 3);
+        }
+
+    }
+}
